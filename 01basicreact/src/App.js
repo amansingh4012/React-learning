e3693f4d7af5e3ded0d87747df40b6cd188d915e
Aman Kumar Singh <123794954+amansingh4012@users.noreply.github.com>
2025-08-15 22:57:59 +0530

    commit

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,10 @@
+
+
+
+function App() {
+  return (
+    <h1>LET's LEARN React</h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,19 @@
+import Chai from "./chai"
+
+
+
+function App() {
+ 
+
+  return (
+    <> 
+
+    <Chai/>
+    <h1>good morning</h1>
+
+    </>
+  
+  )
+}
+
+export default App
